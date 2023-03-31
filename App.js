f733140915f87d5c6d817323568a1aedc50825dc
ahmedsami666 <asz123912@gmail.com>
2023-04-01 00:07:14 +0200

    second

--- App.js
+++ App.js
@@ -29,6 +29,6 @@ app.get("/Website-Team", function(req, res){
     res.render("Website-Team")
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000")
 });
